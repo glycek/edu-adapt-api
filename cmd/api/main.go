fe7648842fbf18e63eec99e63da8652f46f092ed
glycek <glycekk@gmail.com>
2025-11-25 01:31:21 +0300

    Clean up Swagger comments in main.go
    Removed commented-out Swagger documentation and related imports.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,22 +6,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/glycek/edu-adapt-api/config"
-	// Драйвер для Swagger (пока пустой импорт, чтобы не ругался)
-	//_ "github.com/glycek/edu-adapt-api/docs"
 )
 
-// @title Edu-Adapt API
-// @version 1.0
-// @description API для управления учебными материалами
-// @host localhost:8080
-// @BasePath /
 func main() {
 
 	cfg := config.MustLoad()
-	// Инициализируем Gin (роутер)
 	router := gin.Default()
 
-	// Health-check (чтобы проверить, что всё работает)
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
